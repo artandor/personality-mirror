1f5cabdf69ada9dc21ebfe12f1ac02fb93453df9
Nicolas MYLLE <millt.nico@hotmail.fr>
2025-04-23 15:32:41 +0200

    Fix image export

--- app/api/story-image/route.js
+++ app/api/story-image/route.js
@@ -1,44 +1,29 @@
 // app/api/story-image/route.js
-import satori from "satori";
-import { Resvg } from "@resvg/resvg-wasm";
-import { ensureResvgInitialized } from "../../../lib/init-resvg.js";
-
-import { NextResponse } from "next/server";
+import { ImageResponse } from "@vercel/og";
 import StoryImage from "../../../components/StoryImage";
 import React from "react";
-import { readFile } from "fs/promises";
+
+export const runtime = "edge";
 
 export async function POST(req) {
-  const fontPath = `${process.cwd()}/public/fonts/ELEGANTTYPEWRITERRegular.ttf`;
-  const fontData = await readFile(fontPath);
-
-  await ensureResvgInitialized();
-
   const body = await req.json();
   const { traits, idealTraits, summary, people } = body;
 
-  const svg = await satori(
-    <StoryImage traits={traits} idealTraits={idealTraits} summary={summary} people={people} />, 
+  return new ImageResponse(
+    <StoryImage traits={traits} idealTraits={idealTraits} summary={summary} people={people} />,
     {
       width: 1080,
       height: 1920,
       fonts: [
         {
           name: "Inter",
-          data: fontData,
+          data: await fetch(
+            new URL("../../../public/fonts/ELEGANTTYPEWRITERRegular.ttf", import.meta.url)
+          ).then((res) => res.arrayBuffer()),
           style: "normal",
           weight: 400
         }
       ]
     }
   );
-
-  const png = new Resvg(svg).render().asPng();
-
-  return new NextResponse(png, {
-    headers: {
-      "Content-Type": "image/png",
-      "Content-Disposition": "inline; filename=story.png"
-    }
-  });
 }
